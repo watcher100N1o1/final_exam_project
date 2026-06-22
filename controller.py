27c524e202d8eb2ed55f9060aaed3f5faa1c1946
flesh0of0time <nk@Neot.Mchn_spir.com>
2026-06-22 21:34:14 +0300

    full working v1

--- controller.py
+++ controller.py
@@ -1,46 +1,67 @@
+"""**Файл-контроллер**
 
-from model import create_record, delete_record, show_records, record_to_file, load_from_json_file
-from view import show_menu
+
+Здесь описывается взаимодействие моделей, описываемых в model.py"""
+
+
+#
+from model import create_record, delete_record, record_to_file, load_from_json_file
+#
+from view import show_menu, show_records
+
 
 def main():
+    """**Основная функция**
+    
+    Рабочее тело проекта"""
+
+
+    #Список записей (пока пустой)
     data = []
+
+    #Бесконечный цикл:
     while True:
+        #Выводим меню:
         show_menu()
 
+        #Получаем ввод выбора с клавиатуры пользователя:
         choice = input("> ")
 
-
+        #Если выбор - 1 - создаем новую запись и добавляем в список
         if choice == "1":
             create_record(data)
 
-
+        #Если выбор - 2 - удаляем запись из списка
         elif choice == "2":
             delete_record(data)
 
-
+        #Если выбор - 3 - красиво выводим все записи
         elif choice == "3":
             show_records(data)
 
-
+        #Если выюор - 4 - записываем все данные из списка в файл data.json
         elif choice == "4":
             record_to_file("data.json", data)
 
-
+        #Если выбор - 5 - загружаем в список данные из файла data.json
         elif choice == "5":
             data = load_from_json_file("data.json")
         
-
+        #Если выбор 6 - выводим сообщения и прерываем список.
         elif choice == "6":
             print("Завершение программы...")
             print("Всего доброго!")
             break
 
-
+        #Иначе: выводим сообщения
         else:
-            print("Ошибка: невершная команда!")
+            print("Ошибка: неверная команда!")
             print("Попробуйте ввести одну из предложенных комманд.")
 
+
+#Пытаемся запустить программу:
 try:
     main()
+#Если программа "грубо завершается" - выводим сообщения.
 except KeyboardInterrupt:
     print("\nЗавершение программы...")
--- model.py
+++ model.py
@@ -1,23 +1,60 @@
-import random
+"""**Файл-модель**
+
+Здесь хранятся все классы и функции, так или иначе описывающие логику работа
+программы"""
+
+
+#Импортируем необходимые для работы элементы из сторонних библиотек.
+from random import choice
 import json
 import string
 from datetime import datetime
 
+
 def create_password(len_password:int):
-    base_str = string.ascii_letters + string.digits
-    password_str = ""
-    for _ in range(len_password):
-        password_str += random.choice(base_str)
+    """**Генератор-пароля**
     
+    Вносим len_password - длинну пароля."""
+
+    #Строка, состоящая из заглавныъ/строчных латинских букв.
+    #а также цифр
+    base_str = string.ascii_letters + string.digits
+    #Пустая строка-база для пароляю
+    password_str = ""
+
+    #Цикл, повторяющийся len_password количество раз.
+    for _ in range(len_password):
+
+        #Выбираем случайный элемент и добавляем его к строке-базе. 
+        password_str += choice(base_str)
+    
+    #Возращаем сформированную строку.
     return password_str
 
 
 def now_time():
+    """**Функция-часы**
+    
+    Возращает текущее время"""
+
+    #Возращаем текущую дату и время по модели:
+    #день.месяц.год часы:минуты
     return (datetime.now()).strftime("%d.%m.%Y %H:%M")
 
 
 class PasswordRecord:
+    """**Класс-запись**
+    
+    Создан для удобства структурирования данных"""
+
     def __init__(self, service, username, len_password):
+        """**Обьявляем переменные**
+        
+        service - название сервиса/ссылка на сервис (строка)
+        username - имя аккаунта (строка)
+        password - пароль, сгенерированный по длинне (строка)
+        created_at - дата создания записи (строка)
+        """
         self.__service = service 
         self.__username = username
         self.__password = create_password(len_password)
@@ -25,97 +62,107 @@ class PasswordRecord:
 
 
     def get_rec(self):
+        """**Функция -геттер**
+        
+        Позволяет вывести данные в удобном для обработки формате"""
 
+
+        #Создаем словарь с полями, соответствующими переменным, 
+        # и помещаем в них соответствующие переменные.
         data_dict = {
             "Service": self.__service,
             "Username": self.__username,
             "Password": self.__password,
             "CreatedAt": self.__created_at
             }
-        
+
+        #Возращаем словарь.
         return data_dict
 
 
 
 def create_record(base_lst:list):
+    """**Функция-создатель записец**
+    
+    Создает запись и добавляет её в список
+    
+    Вносим base_lst - список"""
 
+
+    #Принимаем ввод названия.
     user = input("Введите имя аккаунта:")
-    service = input("Введите название сервиса/ссылку на сервис:")
+    #Принимаем ввод сервиса.
+    service = input("Введите название сервиса/ссылку на сервис: ")
 
+    #Пытаемся:
     try:
 
-        
+        #Принять ввод длины пароля  и переформатировать его в число.
         len_pass = int(input("Введите длинну пароля:"))
+    
+    #Если было введенро не число - выводим сообщение и возращаем ничего.
     except ValueError:
 
         
         print("Ошибка: введено не число.")
-        print("Попробуйте в следующий раз вводить только цифры.")
+        print("Попробуйте в следующий раз вводить только цифры.") 
         return None
 
+    #Создаем обьем-запись.
     data_rec = PasswordRecord(service, user, len_pass)
+    #Добавляем сформированный в обьекте словарь в списокю
 
     base_lst.append(data_rec.get_rec())
-
-
-def show_records(base_lst:list):
-
-    
-    print("\nЗаписи:")
-    print("="*10)
-
-    if base_lst:
-
-        
-        for rec in base_lst:
-
-            
-            print(f"Юзернейм: {rec["Username"]}")
-            print(f"Сервис: {rec["Service"]}")
-            print(f"Пароль: {rec["Password"]}")
-            print(f"Дата создания: {rec["CreatedAt"]}")
-            print("="*10)
-    else:
-
-        
-        print("Список записей пуст.")
+    #Выводим сообщение.
+    print("Запись добавлена.")
 
 
 def delete_record(base_lst: list):
-
+    """**Функция-удалятор**
     
+    Удаляет элемент из списка по имени
+    
+    Вносим base_lst - список"""
+    
+    #Если список не пуст:
     if base_lst:
 
-        
+        #Принимаем ввод имени:
         need_name = input("Введите юзернейм:")
 
+        #Проходимся по списку:
         for rec in base_lst:
+        
+            #Если поле Username совпадает с введенным именем:
             if rec["Username"] == need_name:
 
-                
+                #Вопрос
                 question = "Вы уверены, что хотите удалить запись(Д/Н): "
+                #Принимаем ответ на вопрос:
                 choice = input(question).lower()
 
-
+                #Если ответ д или l:
                 if choice == "д" or choice == "l":
 
-                    
+                    #Убираем элемент из списка.
                     base_lst.remove(rec)
 
-                    
+                    #Выводим сообщение и завершаем функцию.
                     print("Запись удалена!")
                     return None
 
-                else:
-
-                    
+                #Если ответ - любой другой символ - 
+                # выводим сообщение и также хавершаем функцию.
+                else:   
                     print("Отмена удаления...")
                     return None
 
+        #Иначе - выводим сообщения.
         else:
             print(f"Запись с юзером {need_name} отсутствует в базе данных.")
-            print("Попробуйте другой юзерней.")
+            print("Попробуйте другой юзернейм.")
 
+    #Иначе - выводим сообщение.
     else:
         print("Список записей пуст.")
 
@@ -123,33 +170,52 @@ def delete_record(base_lst: list):
 
 
 def load_from_json_file(URL):
-
+    """**Функция-загрузчик**
     
+    Загружает данные из файла
+    
+    Вносим URL - юрл файла"""
+
+    #Пытаемся:
     try:
 
-        
+        #Открыть файл и поместить его содержимое в переменную data
         with open(URL, "r", encoding="UTF-8") as file:
             data = json.load(file)
 
-        
+        #Вывести сообщение
+        print("Данные получены.")
+        #Возратить переменную data
         return data
 
-    
+    #Если файла с таким URL несуществует:
     except FileNotFoundError:
 
+        #Выводим сообщение
         print("Ошибка: файл несуществует!")
-        print(f"Создание файла {URL}")
+        print(f"Создание файла...")
 
+        #Создаем файл
         file = open(URL, "w", encoding="UTF-8")
         file.close()
 
 
 def record_to_file(URL, base_lst):
-
+    """**Функция-записыватель**
     
+    Функция, записывающая из списка информацию в файл
+    
+    Вносим:
+    URL - юрл файла
+    base_lst - список"""
+    
+    #Открываем файл
     with open(URL, "w", encoding="UTF-8") as file:
 
-        
+        #Записываем данные из списка в файл.
+        #Делаем чтение русских букв возможным.
+        #Задаем 4 отствупа в структуре файла. 
         json.dump(base_lst, file, ensure_ascii=False, indent=4)
+        print("Файл обновлен.")
 
 
